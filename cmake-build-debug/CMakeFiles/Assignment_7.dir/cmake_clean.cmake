1ff64e8dbf9eba27ab19e4b9458fa46457e10c39
RaiymbekNurtay <79619797+RaiymbekNurtay@users.noreply.github.com>
2021-11-11 17:28:20 +0600

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/Assignment_7.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "Assignment_7.exe"
+  "Assignment_7.pdb"
+  "CMakeFiles/Assignment_7.dir/Text_preprocessor.cpp.o"
+  "CMakeFiles/Assignment_7.dir/main.cpp.o"
+  "libAssignment_7.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Assignment_7.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
